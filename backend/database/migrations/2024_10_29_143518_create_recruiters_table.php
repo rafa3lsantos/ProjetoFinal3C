af443b676c925cca3f0dbc8cb9912fa2801d0cb2
Gion <GIONXADREZ@GMAIL.COM>
2024-10-29 16:50:45 -0300

    sim

--- backend/database/migrations/2024_10_29_143518_create_recruiters_table.php
+++ backend/database/migrations/2024_10_29_143518_create_recruiters_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('recruiters', function (Blueprint $table) {
             $table->id();
+            
             $table->timestamps();
         });
     }
