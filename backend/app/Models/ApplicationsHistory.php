cf7743f568575d18457da6f026735cfc1b4d43c7
rafa3lsantos <rafaelsantosdoamaral07@gmail.com>
2024-10-31 13:51:37 -0300

    Models, migrations e controllers criados

---
+++ backend/app/Models/ApplicationsHistory.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ApplicationsHistory extends Model
+{
+    use HasFactory;
+}
