d4ac9829b2fa6acc63bb64064b1361067eb52e9f
CassioFranco <engs-cassiofranco@camporeal.edu.br>
2024-11-06 13:12:50 -0300

    registro

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,16 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from "./store";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import axios from 'axios';
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
+axios.defaults.baseURL = 'App\Http\Controllers\CandidateController@store';
+axios.defaults.withCredentials = true;
 
 
+const app = createApp(App);
 
-const app = createApp(App)
+app.use(router);
+app.use(store);
 
-app.use(router)
-app.use(store)
+app.config.globalProperties.$axios = axios;
 
-app.mount('#app')
+app.mount('#app');
--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -13,58 +13,51 @@
                                 <div class="col-md-6 col-lg-7 d-flex align-items-center">
                                     <div class="card-body p-4 p-lg-5 text-black">
 
-                                        <form>
+                                        <form @submit.prevent="registerCandidate">
 
                                             <h5 class="fw-normal mb-2 pb-3" style="letter-spacing: 1px;">Registre-se:
                                             </h5>
 
-                                            <div data-mdb-input-init class="form-outline mb-2">
-                                                <label class="form-label" for="form2Example17">Nome Completo</label>
-                                                <input type="email" id="form2Example17"
-                                                    class="form-control form-control" />
+                                            <div class="form-outline mb-2">
+                                                <label class="form-label">Nome Completo</label>
+                                                <input type="text" v-model="name" class="form-control" />
                                             </div>
 
-                                            <div data-mdb-input-init class="form-outline mb-2">
-                                                <label class="form-label" for="form2Example17">CPF</label>
-                                                <input type="email" id="form2Example17"
-                                                    class="form-control form-control" />
+                                            <div class="form-outline mb-2">
+                                                <label class="form-label">CPF</label>
+                                                <input type="text" v-model="cpf" class="form-control" />
                                             </div>
 
-                                            <div data-mdb-input-init class="form-outline mb-2">
-                                                <label class="form-label" for="form2Example17">Email</label>
-                                                <input type="email" id="form2Example17"
-                                                    class="form-control form-control" />
+                                            <div class="form-outline mb-2">
+                                                <label class="form-label">Email</label>
+                                                <input type="email" v-model="email" class="form-control" />
                                             </div>
 
-                                            <div data-mdb-input-init class="form-outline mb-2">
-                                                <label class="form-label" for="form2Example27">Senha</label>
-                                                <input type="password" id="form2Example27"
-                                                    class="form-control form-control" />
+                                            <div class="form-outline mb-2">
+                                                <label class="form-label">Senha</label>
+                                                <input type="password" v-model="password" class="form-control" />
                                             </div>
 
-                                            <div data-mdb-input-init class="form-outline mb-2">
-                                                <label class="form-label" for="form2Example17">Repetir Senha</label>
-                                                <input type="email" id="form2Example17"
-                                                    class="form-control form-control" />
+                                            <div class="form-outline mb-2">
+                                                <label class="form-label">Repetir Senha</label>
+                                                <input type="password" v-model="password_confirmation"
+                                                    class="form-control" />
                                             </div>
 
                                             <div class="form-check d-flex justify-content-center mb-3">
-                                                <input class="form-check-input me-2" type="checkbox" value=""
-                                                    id="form2Example3cg" />
-
-                                                <label class="form-check-label" for="form2Example3g">
+                                                <input class="form-check-input me-2" type="checkbox"
+                                                    v-model="termsAccepted" />
+                                                <label class="form-check-label">
                                                     Concordo com os <a href="#!" class="text-body"><u>Termos de
                                                             serviço</u></a>
                                                 </label>
                                             </div>
-                                            <div class="d-flex justify-content-center">
-                                                <router-link to="/home-candidato"
-                                                    class="btn btn-success btn-block btn-lg gradient-custom-4 text-body"
-                                                    data-mdb-button-init data-mdb-ripple-init>
-                                                    Registre-se
-                                                </router-link>
-                                            </div>
 
+                                            <div class="d-flex justify-content-center">
+                                                <button type="submit" class="btn btn-success btn-block btn-lg">
+                                                    Registre-se
+                                                </button>
+                                            </div>
 
                                             <p class="text-center text-muted mt-4 mb-0">
                                                 É uma empresa?
@@ -86,13 +79,51 @@
             </div>
         </section>
     </div>
-
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
+    data() {
+        return {
+            name: '',
+            cpf: '',
+            email: '',
+            password: '',
+            password_confirmation: '',
+            termsAccepted: false,
+        };
+    },
+    methods: {
+        async registerCandidate() {
+            if (!this.termsAccepted) {
+                alert('Você deve aceitar os termos de serviço.');
+                return;
+            }
 
-}
+            try {
+                const response = await axios.post('/candidate/register', {
+                    name: this.name,
+                    cpf: this.cpf,
+                    email: this.email,
+                    password: this.password,
+                    password_confirmation: this.password_confirmation,
+                });
+
+                this.$router.push('/home-candidato');
+            } catch (error) {
+
+                if (error.response && error.response.data) {
+                    console.error("Erro ao registrar o candidato:", error.response.data);
+                    alert(`Erro: ${error.response.data.message || 'Verifique os dados e tente novamente.'}`);
+                } else {
+                    console.error("Erro ao registrar o candidato:", error);
+                    alert('Erro ao registrar. Verifique os dados e tente novamente.');
+                }
+            }
+        }
+    }
+
+};
 </script>
-
-<style></style>
