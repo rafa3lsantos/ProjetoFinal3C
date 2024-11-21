ead2537d94fd07bd7edccf659ad05f369f16a36a
CassioFranco <engs-cassiofranco@camporeal.edu.br>
2024-11-21 13:26:00 -0300

    form de vagas completo

--- frontend/src/views/AddVagas.vue
+++ frontend/src/views/AddVagas.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <NavbarRecrutador/>
+        <NavbarRecrutador />
 
         <div class="container p-0">
             <div class="row">
@@ -12,7 +12,8 @@
                         <div class="list-group list-group-flush" role="tablist">
                             <router-link to="/perfil-recrutador"
                                 class="list-group-item list-group-item-action">Conta</router-link>
-                            <router-link to="/add-vaga" class="list-group-item list-group-item-action">Adicionar Vaga</router-link>
+                            <router-link to="/add-vaga" class="list-group-item list-group-item-action">Adicionar
+                                Vaga</router-link>
                         </div>
                     </div>
                 </div>
@@ -23,48 +24,48 @@
                             <h5 class="card-title mb-0">Adicionar Vaga</h5>
                         </div>
                         <div class="card-body">
-                            <form @submit.prevent="updateConta">
-                                <div class="row">
-                                    <div class="col-md-8">
-                                        <div class="form-group">
-                                            <label for="name">Nome Completo</label>
-                                            <input type="text" class="form-control" id="name"
-                                                v-model="usuario.name_candidate" placeholder="Nome Completo">
-                                        </div>
-                                        <div class="form-group">
-                                            <label>Gênero</label>
-                                            <div class="genero-options py-2">
-                                                <div class="form-check" v-for="(option, index) in genderOptions"
-                                                    :key="index">
-                                                    <input class="form-check-input" type="radio" :name="'gender'"
-                                                        :id="option.value" :value="option.value"
-                                                        v-model="usuario.gender">
-                                                    <label class="form-check-label" :for="option.value">{{ option.label
-                                                        }}</label>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="phone">Telefone</label>
-                                            <input type="text" class="form-control" id="phone" v-model="usuario.phone"
-                                                placeholder="Informe seu Telefone" />
+                            <form @submit.prevent="createJob">
+                                <div class="form-group">
+                                    <label for="title">Título da Vaga</label>
+                                    <input type="text" class="form-control" id="title" v-model="vaga.title"
+                                        placeholder="Título da vaga que será exibido">
+                                </div>
+                                <div class="form-group">
+                                    <label>Modelo de Trabalho</label>
+                                    <div class="modelo-options py-2">
+                                        <div class="form-check" v-for="(option, index) in modeloOptions" :key="index">
+                                            <input class="form-check-input" type="radio" :id="option.value"
+                                                :value="option.value" v-model="vaga.work_model">
+                                            <label class="form-check-label" :for="option.value">{{ option.label
+                                                }}</label>
                                         </div>
                                     </div>
-                                    <div class="col-md-4">
-                                        <div class="text-center">
-                                            <div class="mt-2">
-                                                <img class="rounded-circle mt-5" width="150px"
-                                                    :src="usuario.perfilPicture" alt="Imagem de Perfil">
-                                                <input type="file" @change="onImageChange" style="display: none;"
-                                                    ref="fileInput" />
-                                                <button type="button" class="btn btn-primary mt-3"
-                                                    @click="triggerFileInput">Alterar Imagem</button>
-
-                                            </div>
-                                            <small>Adicione uma foto de perfil para seu recrutador. Se não selecionar,
-                                                será usada a imagem padrão.</small>
+                                </div>
+                                <div class="form-group">
+                                    <label>Tipo de Trabalho</label>
+                                    <div class="tipo-options py-2">
+                                        <div class="form-check" v-for="(option, index) in tipoOptions" :key="index">
+                                            <input class="form-check-input" type="radio" :id="option.value"
+                                                :value="option.value" v-model="vaga.job_type">
+                                            <label class="form-check-label" :for="option.value">{{ option.label
+                                                }}</label>
                                         </div>
                                     </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="state">Estado</label>
+                                    <input type="text" class="form-control" id="state" v-model="vaga.jobs_state"
+                                        placeholder="Estado da vaga">
+                                </div>
+                                <div class="form-group">
+                                    <label for="city">Cidade</label>
+                                    <input type="text" class="form-control" id="city" v-model="vaga.jobs_city"
+                                        placeholder="Cidade da vaga">
+                                </div>
+                                <div class="form-group">
+                                    <label for="description">Sobre a Vaga</label>
+                                    <textarea rows="4" class="form-control" id="description" v-model="vaga.description"
+                                        placeholder="Descrição detalhada da vaga, que o candidato gostaria de saber."></textarea>
                                 </div>
                                 <button type="submit" class="btn btn-primary">Salvar Informações</button>
                             </form>
@@ -79,8 +80,6 @@
 <script>
 import NavbarRecrutador from '@/components/NavbarRecrutador.vue';
 import HttpService from '../services/HttpService';
-import { mapGetters } from 'vuex';
-
 
 export default {
     components: {
@@ -88,111 +87,72 @@ export default {
     },
     data() {
         return {
-            usuario: {
-                id: '',
-                name_candidate: '',
-                about_candidate: '',
-                phone: '',
-                gender: '',
-                perfilPicture: 'https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg'
+            vaga: {
+                title: '',
+                work_model: '',
+                job_type: '',
+                jobs_state: '',
+                jobs_city: '',
+                description: ''
             },
-            profileImagePreview: '',
-            genderOptions: [
-                { label: 'Masculino', value: 'masculino' },
-                { label: 'Feminino', value: 'feminino' },
-                { label: 'Não-Binário', value: 'nao-binario' },
-                { label: 'Outro', value: 'outro' },
-                { label: 'Prefiro não responder', value: 'sem-resposta' }
+            modeloOptions: [
+                { label: 'Presencial', value: 'presential' },
+                { label: 'Remoto', value: 'remote' },
+                { label: 'Híbrido', value: 'hybrid' }
             ],
-            errors: {},
+            tipoOptions: [
+                { label: 'Efetivo', value: 'effective' },
+                { label: 'Freelancer', value: 'freelancer' },
+                { label: 'Temporário', value: 'temporary' },
+                { label: 'Estágio', value: 'internship' }
+            ],
             token: localStorage.getItem('authToken') || ''
         };
     },
-    created() {
-        this.fetchUserProfile();
-    },
-    computed: {
-        ...mapGetters(['getCandidateId'])
-    },
     methods: {
-        onImageChange(event) {
-            const file = event.target.files[0];
-            if (file) {
-                this.profileImagePreview = URL.createObjectURL(file);
-                const reader = new FileReader();
-                reader.onload = () => {
-                    this.usuario.perfilPicture = reader.result;
-                };
-                reader.readAsDataURL(file);
-            }
-        },
-        validateFields() {
-            this.errors = {};
-            if (!this.usuario.name_candidate) {
-                this.errors.name_candidate = "O nome é obrigatório.";
-            }
-            if (!this.usuario.gender) {
-                this.errors.gender = "O gênero é obrigatório.";
-            }
-            return Object.keys(this.errors).length === 0;
-        },
-        async sendUpdateRequest() {
-            try {
-                if (!this.token) {
-                    alert('Usuário não autenticado!');
-                    return;
-                }
+        async createJob() {
+            if (!this.validateFields()) return;
 
-                const response = await HttpService.put(`/candidate/update/${this.getCandidateId}`, this.usuario, {
+            try {
+                const response = await HttpService.post('/jobs/register', this.vaga, {
                     headers: {
-                        'Authorization': `Bearer ${this.token}`,
+                        Authorization: `Bearer ${this.token}`,
                         'Content-Type': 'application/json'
                     }
                 });
 
-                console.log(response)
-
-                if (response.status == 200) {
-                    alert('Informações da conta atualizadas com sucesso.');
+                if (response.status === 201) {
+                    alert('Vaga criada com sucesso!');
+                    this.clearForm();
                 } else {
-                    alert('Erro ao atualizar as informações do candidato.');
+                    alert('Erro ao criar a vaga. Tente novamente.');
                 }
             } catch (error) {
-                console.error("Erro ao atualizar conta:", error);
-                alert('Erro ao atualizar informações da conta.');
+                console.error('Erro ao criar vaga:', error);
+                alert('Erro ao criar a vaga.');
             }
         },
-        async updateConta() {
-            if (this.validateFields()) {
-                await this.sendUpdateRequest();
+        validateFields() {
+            if (!this.vaga.title || !this.vaga.work_model || !this.vaga.job_type || !this.vaga.jobs_state || !this.vaga.jobs_city || !this.vaga.description) {
+                alert('Preencha todos os campos!');
+                return false;
             }
+            return true;
         },
-        async fetchUserProfile() {
-            try {
-                const response = await HttpService.get(`/candidate/show/${this.getCandidateId}`, {
-                    headers: {
-                        Authorization: `Bearer ${this.token}`
-                    }
-                });
-                const user = response.data.candidate;
-                this.usuario.name_candidate = user.name_candidate || '';
-                this.usuario.phone = user.phone || '';
-                this.usuario.gender = user.gender || '';
-                this.usuario.about_candidate = user.about_candidate || '';
-                this.usuario.perfilPicture = user.perfilPicture || this.usuario.perfilPicture;
-            } catch (error) {
-                console.error('Erro ao carregar o perfil do usuário:', error);
-            }
-        },
-        triggerFileInput() {
-            this.$refs.fileInput.click();
+        clearForm() {
+            this.vaga = {
+                title: '',
+                work_model: '',
+                job_type: '',
+                jobs_state: '',
+                jobs_city: '',
+                description: ''
+            };
         }
-    },
-    mounted() {
-        this.usuario.id = this.getCandidateId;
     }
 };
 </script>
+
 
 <style scoped>
 body {
@@ -219,6 +179,7 @@ body {
 
 .form-check {
     margin-right: 15px;
+    
 }
 
 .form-group {
@@ -228,5 +189,11 @@ body {
 .text-center small {
     display: block;
     margin-top: 10px;
+}
+
+.tipo-options,
+.modelo-options {
+    display: flex;
+    flex-wrap: wrap;
 }
 </style>
