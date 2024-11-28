ad0dd0c2f0bab20bf879e8db0d330c225a81fc28
Gion <GIONXADREZ@GMAIL.COM>
2024-11-28 17:06:48 -0300

    quero sim

--- backend/app/Http/Controllers/FormationController.php
+++ backend/app/Http/Controllers/FormationController.php
@@ -15,29 +15,27 @@ class FormationController extends Controller
 
     // Método para criar uma nova formação
     public function store(Request $request)
-    {
-        $arrayRequest = $request->validate([
-            'formation' => 'required|string|max:255',
-            'institution' => 'required|string|max:255',
-            'experience' => 'required|string|max:500',
-            'degree' => 'required|string|in:tecnologo,licenciatura,bacharelado',
-            'status' => 'required|string|in:completo,em andamento,incompleto',
-            'course' => 'required|string|max:255',
-            'start_date_course' => 'required|date',
-            'end_date_course' => 'required|date|after_or_equal:start_date_course',
-            'certificate_type' => 'required|string|in:conquista,certificado',
-            'certificate_title' => 'required|string|max:255',
-            'certificate_description' => 'required|string|max:500',
-            'certificate_institution' => 'required|string|max:255',
-        ]);
+{
+    $validatedData = $request->validate([
+        'formation' => 'required|string',
+        'institution' => 'required|string',
+        'experience' => 'required|string',
+        'degree' => 'required|string',
+        'status' => 'required|string',
+        'course' => 'required|string',
+        'start_date_course' => 'required|date',
+        'end_date_course' => 'nullable|date',
+        'certificate_type' => 'nullable|string',
+        'certificate_title' => 'nullable|string',
+        'certificate_description' => 'nullable|string',
+        'certificate_institution' => 'nullable|string',
+        'candidate_id' => 'required|exists:candidates,id', // Validação para candidate_id
+    ]);
 
-        $formation = Formation::create($arrayRequest);
+    $formation = Formation::create($validatedData);
 
-        return response()->json([
-            'message' => 'Formation registered successfully',
-            'formation' => $formation,
-        ], 201); // Código 201 para criação bem-sucedida
-    }
+    return response()->json($formation, 201);
+}
 
     // Método para atualizar uma formação existente
     public function update(Request $request, $id)
